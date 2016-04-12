c027c32ed31401c74d9bd4b1d31085152c9a0957
dououFullstack <13412133311@139.com>
2016-04-12 10:21:15 +0800

    first commit

--- server/methods/wechat.js
+++ server/methods/wechat.js
@@ -1,7 +1,7 @@
 Meteor.methods({
   getUserInfo: function(code, state) {
-    var appId = 'wx63afd8910ac82737';
-    var appSecret = '06abca4d83a582c46a37957c4c499ba2';
+    var appId = 'wx5585e55977e34370';
+    var appSecret = '3ce737520a8025319029b06b92946e5b';
 
     try {
       var result = HTTP.get("https://api.weixin.qq.com/sns/oauth2/access_token?" +
